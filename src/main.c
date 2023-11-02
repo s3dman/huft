c95cb071183cb902d2b901c311d68218782f2682
snaupdog <nihal.satish@gmail.com>
2023-11-02 12:06:36 +0530

    background

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(BLUE);
+        ClearBackground(BLACK);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
