f2b50dff26daf817a7d6aa313df8e6a89d452b4e
snaupdog <nihal.satish@gmail.com>
2023-11-02 12:14:08 +0530

    text color-green

--- src/main.c
+++ src/main.c
@@ -8,7 +8,7 @@ int main(void)
     {
         BeginDrawing();
         ClearBackground(BLACK);
-        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, GREEN);
         EndDrawing();
     }
 
