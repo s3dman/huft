0477e5db63364b1a8cce59d6f30cae2fd42e4dea
NevilleJS <69287021+s3dman@users.noreply.github.com>
2023-11-02 12:03:28 +0530

    bruh

--- src/main.c
+++ src/main.c
@@ -7,7 +7,7 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLUE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
         EndDrawing();
     }
